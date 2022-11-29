40c8bec87b2040da6a9a78519b22f46b2d634b31
Dennis Prinse <online+github@dennisprinse.com>
2022-11-29 19:57:39 +0100

    Create /usr/local/bin/ if it doesn't exist

--- installation/macos/development-tools.sh
+++ installation/macos/development-tools.sh
@@ -10,3 +10,8 @@ brew_packages_optional='htop jq rsync shellcheck unzip openssl'
 echo 'Installing packages'
 # shellcheck disable=SC2086
 brew install ${brew_packages} ${brew_packages_optional}
+
+if [ ! -d '/usr/local/bin/' ]; then
+  echo 'Create /usr/local/bin/ directory'
+  mkdir -p '/usr/local/bin/'
+fi
