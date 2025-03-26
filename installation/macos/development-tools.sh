e7d581327a7b1d04c5762ae924f5d6e0626b4d18
Dennis Prinse <online+github@dennisprinse.com>
2025-03-26 09:59:00 +0100

    Install missing and gnu counterparts for bsd tools, replace out-of-date tools on macOS

--- installation/macos/development-tools.sh
+++ installation/macos/development-tools.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-brew_packages='diffutils curl git screen tmux vim zsh'
-brew_packages_optional='htop jq rsync shellcheck unzip openssl'
+brew_packages='curl tmux zsh'
+brew_packages_bsd_missing='watch wget wdiff autoconf'
+brew_packages_bsd_counterparts='coreutils binutils diffutils ed findutils gawk gnu-indent gnu-sed gnu-tar gnu-which grep gzip screen grep gpatch'
+brew_packages_bsd_outdated='flex'
+brew_packages_gnu_outdated='bash emacs gpatch less m4 make nano bison'
+brew_packages_gnu_preferred='libressl file-formula git openssh perl python rsync unzip vim'
+brew_packages_optional='htop jq shellcheck openssl'
 
 # -----------------------------------------------
 # Programming Languages, Frameworks and CLI tools
@@ -9,7 +14,13 @@ brew_packages_optional='htop jq rsync shellcheck unzip openssl'
 
 echo 'Installing packages'
 # shellcheck disable=SC2086
-brew install ${brew_packages} ${brew_packages_optional}
+brew install ${brew_packages} \
+  ${brew_packages_bsd_missing} \
+  ${brew_packages_bsd_counterparts} \
+  ${brew_packages_bsd_outdated} \
+  ${brew_packages_gnu_outdated} \
+  ${brew_packages_gnu_preferred} \
+  ${brew_packages_optional}
 
 if [ ! -d '/usr/local/bin/' ]; then
   echo 'Create /usr/local/bin/ directory'
